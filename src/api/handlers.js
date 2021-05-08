de12411f3e8f892252b03a39cb9f1402967c970b
Anshul <anshul.bansal@tikaj.com>
2021-05-08 09:44:35 +0530

    Added conditional to send actual error if dev env

--- src/api/handlers.js
+++ src/api/handlers.js
@@ -4,8 +4,8 @@ function notFoundErrorHandler(req, res) {
   res.dispatch(new NotFound(`Request resource ${req.url} was not found`));
 }
 
-function genericErrorHandler(req, res) {
-  res.dispatch(new ServerError("Internal server error"));
+function genericErrorHandler(err, req, res, next) {
+  res.dispatch(new ServerError(process.env.NODE_ENV === "dev" ? err : null));
 }
 
 module.exports = { notFoundErrorHandler, genericErrorHandler };
