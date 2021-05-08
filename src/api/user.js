11164b988555e81b4424e914c6b3fb3080921c5d
Anshul <anshul.bansal@tikaj.com>
2021-05-08 17:34:08 +0530

    Multiple Changes
    - Added /logout route
    - Using new response objects
    - Removed /usernameTaken route

--- src/api/user.js
+++ src/api/user.js
@@ -1,53 +1,41 @@
 const { Router } = require("express");
 
 const { User } = require("../entities/index.js");
-const { createSession } = require("./../auth/index.js");
+const {
+  createSession,
+  destroySession,
+  authenticate,
+} = require("./../auth/index.js");
+const Response = require("./../response/index.js");
+const { OK, Unauthorized } = Response;
 
 const router = Router();
 
 // Authentication Middlewares
 
-const usernameTaken = async (username) => {
-  return !!(await User.findOne({ username }));
-};
-
-router.get("/usernameTaken/:username", async (req, res, next) => {
-  try {
-    if (await usernameTaken(req.params.username))
-      res.json({
-        status: "OK",
-        data: true,
-      });
-    else {
-      res.json({
-        status: "OK",
-        data: false,
-      });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
-
   try {
     const user = await User.findOne({ username });
     if (await user.isValidPassword(password)) {
       const token = await createSession({ username });
-      res.send({
-        token,
-      });
+      res.dispatch(new OK({ token }));
     } else {
-      res.status(401);
-      throw new Error("Unauthorized");
+      res.dispatch(new Unauthorized("Incorrect credentials"));
     }
   } catch (err) {
     return next(err);
   }
 });
 
-router.get("/logout", async (req, res, next) => {});
+router.get("/logout", authenticate(), async (req, res, next) => {
+  try {
+    console.log(req.token);
+    await destroySession(req.token);
+    res.dispatch(new OK());
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
