50fb150d9205689ca64a320113f00f4168b0ca6f
Anshul <anshul.bansal@tikaj.com>
2021-05-08 09:42:10 +0530

    Added authentication response delegation option

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,33 +1,42 @@
 const jwt = require("jsonwebtoken");
 
 const { Session } = require("./../entities/index.js");
+const Response = require("./../response/index.js");
+const { Unauthorized } = Response;
 
-async function authenticateRequest(req, res, next) {
-  const authHeader = req.header("Authorization");
-  const token = authHeader?.split(" ")[1];
-
-  // Check if no token
-  if (!token) {
-    res.status(401);
-    // TODO: Implement a status property on error object
-    return next(new Error("Unauthorized"));
-  }
-
-  try {
-    // Verfiy token and check in sessions as well
-    const data = jwt.verify(token, process.env.SECRET_KEY);
-
-    if (await Session.findOne({ _id: token })) {
-      req.user = data;
-      next();
-    } else {
-      throw new Error("Internal Server Error");
+function authenticate(delegateResponse = false) {
+  return async function (req, res, next) {
+    function _next() {
+      if (!delegateResponse) {
+        res.dispatch(new Unauthorized());
+        return;
+      } else {
+        next();
+      }
     }
-  } catch (err) {
-    // Currently all errs in authenticateRequest handler will have 401
-    res.status(401);
-    return next(err);
-  }
+
+    const authHeader = req.header("Authorization");
+    const token = authHeader?.split(" ")[1];
+
+    if (!token) {
+      _next();
+      return;
+    }
+
+    try {
+      const data = jwt.verify(token, process.env.SECRET_KEY);
+
+      if (await Session.findOne({ _id: token })) {
+        req.user = data;
+        next();
+      } else {
+        _next();
+      }
+    } catch (err) {
+      if (err.name === "JsonWebTokenError") _next();
+      else next(err);
+    }
+  };
 }
 
 async function createSession(payload) {
@@ -41,4 +50,4 @@ async function createSession(payload) {
   return token;
 }
 
-module.exports = { authenticateRequest, createSession };
+module.exports = { createSession, authenticate };
