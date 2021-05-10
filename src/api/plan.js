248df2475fc28701d64b58fca1571172b983b3d4
Anshul <anshul.bansal@tikaj.com>
2021-05-10 21:25:46 +0530

    Added basic api endpoints for plan

--- src/api/plan.js
+++ src/api/plan.js
@@ -1,28 +1,64 @@
 const { Router } = require("express");
 
+const Plan = require("./../entities/index.js");
+
+const Response = require("../response/index.js");
+const { authenticate } = require("../auth/index.js");
+
 const router = Router();
+const { BadRequest, OK } = Response;
 
 // Authentication Middlewares
 
 // CREATE
-router.post("/", async (req, res, next) => {});
+router.post("/", authenticate(), async (req, res, next) => {
+  const { title, notes, meta, slots } = req.body;
+
+  // Check if meta object is valid
+
+  try {
+    // TODO: Add owner as well
+    const plan = new Plan({ title, notes, meta, slots });
+
+    await plan.validate();
+    await plan.save();
+
+    // TODO: Include only necessary keys
+    res.dispatch(new OK({ data: plan }));
+  } catch (err) {
+    next(err);
+  }
+});
 
 // READ
-router.get("/:id", (req, res) => {
-  // Try to delete plan from given id contained inside req.params
-  // If not existing raise error
+router.get("/:id", authenticate(),(req, res,next) => {
+  const { id } = req.params;
+
+  try {
+    const plan = Plan.findOne({ id });
+    if (!plan) res.dispatch(new BadRequest(`No plan found with id ${id}`));
+    // TODO: Include Only necessary keys
+    res.dispatch(new OK({ data: plan }));
+  } catch (err) {
+    next(err);
+  }
 });
 
 // UPDATE
-router.patch("/:id", (req, res) => {
-  // Try to update plan with id and plan contained inside req.params & req.body respectively
-  // If not existing raise error
+router.patch("/:id",authenticate(), (req, res, next) => {
+  const { id } = req.params;
+  const { title, notes, meta, slots, pinned } = req.body;
 });
 
 // DELETE
-router.delete("/:id", (req, res) => {
-  // Try to delete plan with id contained inside req.param
-  // If not existing raise error
+router.delete("/:id", authenticate(),(req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    await Plan.delete()
+  } catch (err) {
+    next(err)
+  }
 });
 
 module.exports = router;
