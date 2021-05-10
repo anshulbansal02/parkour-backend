3c76c5cf214d02a2e892e2513206fd4d72c1fa21
Anshul <anshul.bansal@tikaj.com>
2021-05-10 20:25:29 +0530

    Updated Plan and InventoryItem Schemas

--- src/entities/InventoryItem.js
+++ src/entities/InventoryItem.js
@@ -1,7 +1,17 @@
 const { model, Schema } = require("mongoose");
 
 const InventoryItemSchema = new Schema({
-  label: { type: String, required: true },
+  type: { type: String, enum: ["food", "activity"] },
+  label: { type: String, required: true, maxlength: 50 },
+  description: { type: String, maxlength: 2000 },
+  // Generic object, can store any key:value discription of item
+  meta: { type: Object },
+  media: {
+    mimeType: { type: String, enum: ["video", "photo"] },
+    uri: { type: String },
+  },
+  owner: { type: Schema.Types.ObjectId, ref: "Users" },
+  deleted: { type: Boolean, required: true, default: false },
 });
 
 const InventoryItem = model("InventoryItems", InventoryItemSchema);
--- src/entities/Plan.js
+++ src/entities/Plan.js
@@ -1,12 +1,22 @@
 const { model, Schema } = require("mongoose");
 
+const Slot = {
+  label: { type: String, required: true, trim: true, maxlength: 50 },
+  notes: { type: String },
+  items: [Schema.Types.ObjectId],
+};
+
 const PlanSchema = new Schema({
   title: { type: String, required: true },
+  notes: { type: String, maxlength: 500 },
+  meta: { type: Object },
+  slots: { type: [Slot], required: true },
+  owner: { type: Schema.Types.ObjectId, required: true },
+  pinned: { type: Boolean, default: false },
   createdOn: { type: Date, default: new Date() },
   updatedOn: { type: Date },
-  createdBy: { type: Schema.Types.ObjectId, required: true },
-  pinned: { type: Boolean },
 });
+
 const Plan = model("Plans", PlanSchema);
 
 module.exports = Plan;
