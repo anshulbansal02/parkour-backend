96ee65cfec59c61d4ad09d2b5033d5a3ee0fe01c
Anshul <anshul.bansal@tikaj.com>
2021-05-13 21:36:26 +0530

    Multiple changes
    - Wrapped embedded slot with mongoose.Schema
    - Using nanoid _id in PlanSchema
    - Added type property in PlanSchema
    - Added slots & items array validations

--- src/entities/Plan.js
+++ src/entities/Plan.js
@@ -1,20 +1,39 @@
 const { model, Schema } = require("mongoose");
+const { nanoid } = require("nanoid");
 
-const Slot = {
-  label: { type: String, required: true, trim: true, maxlength: 50 },
-  notes: { type: String },
-  items: {
-    type: [Schema.Types.ObjectId],
-    ref: "InventoryItems",
-    required: true,
+function arrayValidator(v) {
+  return Array.isArray(v) && v.length > 0;
+}
+
+const Slot = new Schema(
+  {
+    label: { type: String, required: true, trim: true, maxlength: 50 },
+    notes: { type: String },
+    items: {
+      type: [String],
+      ref: "InventoryItems",
+      required: true,
+      validate: {
+        validator: arrayValidator,
+      },
+    },
   },
-};
+  { _id: false }
+);
 
 const PlanSchema = new Schema({
+  _id: { type: String, default: () => nanoid(12) },
+  type: { type: String, enum: ["diet", "workout"], required: true },
   title: { type: String, required: true },
   notes: { type: String, maxlength: 500 },
   meta: { type: Object },
-  slots: { type: [Slot], required: true },
+  slots: {
+    type: [Slot],
+    required: true,
+    validate: {
+      validator: arrayValidator,
+    },
+  },
   owner: { type: Schema.Types.ObjectId, ref: "Users", required: true },
   pinned: { type: Boolean, default: false },
   createdOn: { type: Date, default: new Date() },
