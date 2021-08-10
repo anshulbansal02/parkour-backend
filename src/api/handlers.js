9d4229fc5b30db3dbabd57543fc025c8e39f5311
anshul-subscribili <87595515+anshul-subscribili@users.noreply.github.com>
2021-08-10 21:36:51 +0530

    Fixes in handlers

--- src/api/handlers.js
+++ src/api/handlers.js
@@ -1,9 +1,9 @@
 function notFoundErrorHandler(req, res) {
-  res.dispatch.NotFound(`Request resource ${req.url} was not found`);
+  res.dispatch.NotFound(`Requested resource ${req.url} was not found`);
 }
 
 function genericErrorHandler(err, req, res, next) {
-  const isDev = process.env.NODE_ENV === "dev";
+  const isDev = process.env.NODE_ENV === "development";
   isDev ? console.log(err) : null;
   res.dispatch.ServerError(isDev ? err.message : null);
 }
