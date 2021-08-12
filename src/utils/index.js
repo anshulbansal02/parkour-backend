d3b4fce2442eede1807a2b80e675cb96f3a27f15
anshul-subscribili <87595515+anshul-subscribili@users.noreply.github.com>
2021-08-12 15:14:18 +0530

    Added generateUsername utility method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-function capitalizeFirstLetter(string) {
+function capitalizeFirstLetter(string = "") {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
@@ -8,10 +8,31 @@ function stringFields(fields, capitalize = false, lastWord = "and") {
   fields = capitalize ? fields.map(capitalizeFirstLetter) : fields;
 
   return fields.reduce((s, v, i, a) => {
-    return (i == a.length - 1) && lastWord
+    return i == a.length - 1 && lastWord
       ? `${s} ${lastWord} ${v}`
       : `${s}, ${v}`;
   });
+}
+
+function randomItem(array) {
+  return array[randomNumber(0, array.length - 1)];
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateUsername(name, maxSeed = 999) {
+  const names = name?.split(" ");
+  const fName = names.splice(0, 1)[0];
+  const rName = names.join("");
+
+  return (
+    randomItem([fName, fName.toLowerCase()]) +
+    randomItem([".", "_", ""]) +
+    randomItem([rName, rName[0], rName.toLowerCase(), ""]) +
+    randomNumber(0, maxSeed)
+  );
 }
 
 class FilterError extends Error {
@@ -36,17 +57,21 @@ function validateFilters(filters, params) {
       case Number:
         params[param] = +params[param];
         if (params[param] < filter.min || params[param] > filter.max) {
-          throw new FilterError(`Query param '${param}' is not in range ${filter.min}-${filter.max}`);
+          throw new FilterError(
+            `Query param '${param}' is not in range ${filter.min}-${filter.max}`
+          );
         }
         break;
       case "Enumerator":
         params[param] = isNaN(+params[param]) ? params[param] : +params[param];
         if (!filter.values.includes(params[param])) {
-          throw new FilterError(`Query param '${param}' is not one of ${stringFields(
-            filter.values,
-            false,
-            "or"
-          )}`);
+          throw new FilterError(
+            `Query param '${param}' is not one of ${stringFields(
+              filter.values,
+              false,
+              "or"
+            )}`
+          );
         }
         break;
     }
@@ -60,4 +85,10 @@ function validateFilters(filters, params) {
   return { ...defaultParams, ...params };
 }
 
-module.exports = { stringFields, validateFilters };
+module.exports = {
+  stringFields,
+  validateFilters,
+  randomItem,
+  randomNumber,
+  generateUsername,
+};
